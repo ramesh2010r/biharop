e9e78f8bbf5b30395e67b4cc66ec3e1c4070ff44
Ramesh Kumar <ramesh2010r@hotmail.com>
2025-10-22 18:26:09 +0530

    Remove warning icon from disclaimer for cleaner look

--- src/components/PredictionGraph.tsx
+++ src/components/PredictionGraph.tsx
@@ -189,10 +189,7 @@ export default function PredictionGraph() {
 
       {/* Disclaimer */}
       <div className="bg-amber-50 border-t border-amber-100 px-6 py-4 mt-6">
-        <p className="text-sm text-gray-700 text-center hindi-text flex items-center justify-center gap-2">
-          <svg className="w-5 h-5 text-amber-600" fill="currentColor" viewBox="0 0 20 20">
-            <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
-          </svg>
+        <p className="text-sm text-gray-700 text-center hindi-text">
           यह एक ओपिनियन पोल आधारित पूर्वानुमान है, आधिकारिक परिणाम नहीं
         </p>
       </div>
